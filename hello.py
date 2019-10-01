90d33c07a469222ed075c6dd471c8a451f1f574c
Daniel Troutman <dtroutm1@vols.utk.edu>
2019-10-01 16:19:46 -0400

    Added functions

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 def helloworld():
-    pass
+    print("Hello")
 
 def add_numbers(x, y):
-    pass
+    return x + y
 
 if __name__ == '__main__':
     # do something!
-    pass
+    helloworld()
