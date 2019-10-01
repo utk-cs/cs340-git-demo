a8197eabb9212223d2e807f4d491f901e27c44a3
Alex Lambert <alambe22@vols.utk.edu>
2019-10-01 16:25:26 -0400

    Fixed adding I thonk :think:

--- hello.py
+++ hello.py
@@ -2,9 +2,11 @@
 
 def helloworld():
     pass
-
 def add_numbers(x, y):
-    pass
+    x1 = x;
+    x2 = y;
+    return (3*(2*(x1+x2 - (x+y) + (x+y)))/3)*2;
+    
 
 if __name__ == '__main__':
     # do something!
