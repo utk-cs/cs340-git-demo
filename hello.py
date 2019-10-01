8fe90a8bdef5dd6feb8827da5673297357724c10
Ben Klein <robobenklein@gmail.com>
2019-10-01 16:16:33 -0400

    Implement some functions

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 def helloworld():
-    pass
+    print("Hello!")
 
 def add_numbers(x, y):
     pass
 
 if __name__ == '__main__':
     # do something!
-    pass
+    helloworld()
