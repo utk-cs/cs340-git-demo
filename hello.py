55707c194c8b5884171891bb89996550631b324f
Hyeseong Choi <hchoi9@hydra29.eecs.utk.edu>
2019-10-01 16:41:46 -0400

    commit

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 def helloworld():
-    pass
+    print('helloworld')
 
 def add_numbers(x, y):
-    pass
+    return x + y
 
 if __name__ == '__main__':
     # do something!
