baa57977152052a706f623d3d6d7180f0356d5d7
Noah Hobson <nhobson@vols.utk.edu>
2019-10-01 16:41:10 -0400

    i think i got it

--- library.py
+++ library.py
@@ -1,7 +1,8 @@
-def turtlestuff:
+def turtlestuff():
     import turtle
     screen = turtle.Screen()
     t = turtle.RawTurtle(screen, 'turtle')
     for i in range(36):
         t.forward(10)
         t.right(10)
+turtlestuff()
