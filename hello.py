6585b072b7ffcc1aa42b3f20bb977301faa3c3cf
Michael Wermert <mwermert@vols.utk.edu>
2019-10-01 16:21:58 -0400

    commiting some stuff

--- hello.py
+++ hello.py
@@ -6,6 +6,16 @@ def helloworld():
 def add_numbers(x, y):
     pass
 
+
+def main():
+
+    var = ""
+    while var != "General Kenobi":
+    
+        var = input("Hello There\n")
+
 if __name__ == '__main__':
     # do something!
-    pass
+    main()
+
+
