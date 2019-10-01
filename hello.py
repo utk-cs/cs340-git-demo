90a516db53d4f049f07457d0d05a20e3a8b342ab
Benjamin Greenberg <bgreenb3@vols.utk.edu>
2019-10-01 16:19:36 -0400

    More fixes

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 def helloworld():
-    pass
+    print("Hello World!")
 
 def add_numbers(x, y):
-    pass
+    return x+y
 
 if __name__ == '__main__':
-    # do something!
-    pass
+    helloworld()
+    add_numbers(40, 2)
